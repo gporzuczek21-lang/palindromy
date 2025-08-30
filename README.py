62095d392f0f0fe244cde9be7ca5c1176e23423f
Gabriela Porzuczek <gporzuczek21@gmail.com>
2025-08-30 22:46:43 +0200

    add definition

--- README.py
+++ README.py
@@ -1,1 +1,6 @@
-# palindromy
+"""
+palindromy
+ten programa sprawdza czy dany wyraz jest palindromem 
+PALINDROM - wyraz, fraza, zdanie lub dowolny ciąg znaków,
+który czytany od lewej do prawej strony brzmi identycznie jak czytany od prawej do lewej strony.
+"""
